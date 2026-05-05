ac5b71529be5c675d196a8e3d4ed277966e89978
SomeDudRice <Lucasl197@nycstudents.net>
2026-05-05 10:17:31 -0400

    preaafafa

---
+++ main.py
@@ -0,0 +1,11 @@
+class prezs:
+    def __init__(president,name,affection,personality):
+        president.name = name
+        president.__affection = affection
+Trump = prezs("Donald trump",-30,"tsundere")
+Biden = prezs("Joe biden", ,"" )
+Obama = prezs("Barrack obama",20,"chill" )
+lincoln = prezs("Abraham lincoln",50,"theater kid" )
+washington = prezs("George washington",20,"wierd childhood friend" )
+jfk = prezs("John F Kennedy",-30,"popular kid" )
+teddy = prezs("Theodore Rossevelt",0,"baseball tomboy" )
