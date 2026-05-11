22c9ec6a899b671cce2f3710ab8f3d17312d0347
dan-te6741 <danieli96@nycstudents.net>
2026-05-11 10:17:10 -0400

    gooey

---
+++ gui.py
@@ -0,0 +1,17 @@
+# import tkinter as tk
+
+# root = tk.Tk()
+# root.mainloop()
+
+import tkinter as tk
+
+root = tk.Tk()
+
+# Setting some window properties
+root.title("Tk Example")
+root.configure(background="yellow")
+root.minsize(200, 200)
+root.maxsize(1000, 1000)
+root.geometry("300x300+50+50")
+
+root.mainloop()
