bbef19fc71e98b513e0fe911fac7cd259f7d0f71
dan-te6741 <danieli96@nycstudents.net>
2026-05-19 10:17:35 -0400

    sywctgh

--- gui.py
+++ gui.py
@@ -1,56 +1,22 @@
-# import tkinter as tk
-
-# root = tk.Tk()
-# root.mainloop()
-
-# import tkinter as tk
-
-# root = tk.Tk()
-
-# # Setting some window properties
-# root.title("Tk Example")
-# root.configure(background="yellow")
-# root.minsize(200, 200)
-# root.maxsize(1000, 1000)
-# root.geometry("300x300+50+50")
-
-# root.mainloop()
-
-# import tkinter as tk
-
-# Create the window
-# root = tk.Tk()
-# root.title("My GUI")
-# root.geometry("300x200")
-
-# # Function that runs when button is clicked
-# def say_hello():
-#     label.config(text="Button clicked!")
-
-# # Create a label
-# label = tk.Label(root, text="Press the button")
-# label.pack(pady=20)
-
-# # Create a button
-# button = tk.Button(
-#     root,
-#     text="Click Me",
-#     command=say_hello
-# )
-
-# button.pack()
-
-# # Start the GUI loop
-# root.mainloop()
 import tkinter as tk
 
-# Create window
+# create window
 root = tk.Tk()
 root.title("Image Button Example")
+root.attributes('-fullscreen',True)
+
+# Load image (must be .png or .gif for standard PhotoImage)
+bg_image = tk.PhotoImage(file="schoolbackground.webp")
+
+# Create label and 'place' it to fill the whole window
+bg_label = tk.Label(root, image=bg_image)
+bg_label.place(relwidth=1, relheight=1)
+
+
 
 # Load image
 photo = tk.PhotoImage(file="button.png")
-small_image = photo.subsample(2,2)
+small_image = photo.subsample(5,5)
 # Function when button is clicked
 def on_click():
     print("Button clicked!")
@@ -58,6 +24,9 @@ def on_click():
 # Create image button
 button = tk.Button(root, image=small_image, command=on_click, borderwidth=0)
 button.pack(pady=20)
+button.place(x=100, y=700)
 
 # Run app
 root.mainloop()
+
+# this code above is an example of how we'll interact with the game. the def function is gonna run certain parts of our game which will be referenced with command. our different buttons will interact with the game
