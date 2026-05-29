5d81516aa433aca8afc835ceae8cff1241b5d08a
dan-te6741 <danieli96@nycstudents.net>
2026-05-29 10:11:40 -0400

    signoutgulp

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
 root = tk.Tk()
-root.title("Image Button Example")
+root.title("Vote Me!")
 root.attributes('-fullscreen',True)
 bg_image = tk.PhotoImage(file="1200.png")
 bg2 = bg_image.zoom(2)
@@ -10,17 +10,14 @@ bg_label.place(relwidth=1, relheight=1)
 # this above code is the background of the game, should be on always
 
 
-# photo = tk.PhotoImage(file="button.png")
-# small_image = photo.subsample(5,5)
+photo = tk.PhotoImage(file="textboxneutral.png")
 
-# def on_click():
-#     print("Button clicked!")
+def on_click():
+    print("Button clicked!")
 
 
-# button = tk.Button(root, image=small_image, command=on_click, borderwidth=0)
-# button.pack(pady=20)
-# button.place(x=100, y=700)
-
-root.mainloop()
-
-# # this code above is an example of how we'll interact with the game. the def function is gonna run certain parts of our game which will be referenced with command. our different buttons will interact with the game
+button = tk.Button(root,  command=on_click, borderwidth=0)
+button.place(x=100, y=700)
+button.pack(pady=20)
+variable676767=root.mainloop()
+variable676767()
