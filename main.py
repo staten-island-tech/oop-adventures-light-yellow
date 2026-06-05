fb6de3319a833f894173115532608ed87edcab85
SomeDudRice <Lucasl197@nycstudents.net>
2026-06-05 09:54:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+lovers=[]
 inputRN = 0
 end = False
 class prezs:
@@ -22,7 +23,7 @@ class prezs:
         president.__affection -=20
         print (president.name,"'s affection for y/n is now",president.__affection,"</3, i hope you get hit by a car" )
     def affection_Cup(president):
-        president.__affection +=100
+        president.__affection +=20
         print (president.name,"'s affection for y/n is now",president.__affection,"<3, the system starts blushing" )
     def confession(president):
         if president.__affection >= 100:
@@ -72,49 +73,50 @@ day_count = 0
 while not end:
     day_count +=1
     print ("day",day_count)
-    for encounters in range(3):
-        random_classmates = random.choice(Classmates)
-        if random_classmates == previous_classmates:
-            while random_classmates == previous_classmates:
-                random_classmates = random.choice(Classmates)
-        print ("you encountered",random_classmates['name'], "in the hallway")
-        inputRN = input('1:hi or 2:ignore')
-        if inputRN =='1':
-            randomchoices = {
-            'good':["i like your shoes y/n-kun","do you want to talk about excuse me sir?","thanks for studying with me twin"],
-            'bad':["want to talk about deepwoken?","can i get your mom's number","on charlie kirk youre chopped"],
-            'supergood':["100 million to isreal?",["i think youre awesome",random_classmates['name'],"-chan"],["i brought your favoritre",random_classmates['likes']]],
-            'superbad':[["i brought your favoritre",random_classmates['dislikes']],"on blue judas imma slime you out","i-i-i-i th-think-nk i-im-m ii-in lov-ve wi-ith y-y-yo-your dad (⸝⸝¬`‸´ ¬⸝⸝)"]
-            }
-            talkitve =+1
-            print ("Hiii~ im",random_classmates['name'],"[insert text and stuff]")
-            for catergories in randomchoices:
-                chosen_text = random.choice(randomchoices[catergories])
-                choosing.append(chosen_text)
-            print("\n".join(
-                f"{i}: {item}"
-                for i, item in enumerate(choosing, start=1)
-            ))
-
-            inputRN = input("what shall you do?")
+    if end == False:
+        for encounters in range(3):
+            random_classmates = random.choice(Classmates)
+            if random_classmates == previous_classmates:
+                while random_classmates == previous_classmates:
+                    random_classmates = random.choice(Classmates)
+            print ("you encountered",random_classmates['name'], "in the hallway")
+            inputRN = input('1:hi or 2:ignore')
             if inputRN =='1':
-                prezs.affection_up(random_classmates["id"])
-            elif inputRN == '2':
-                prezs.affection_down(random_classmates["id"])
-            elif inputRN == '3':
-                prezs.affection_Cup(random_classmates["id"])
-            elif inputRN == '4':
-                prezs.affection_Cdown(random_classmates["id"])
+                randomchoices = {
+                'good':["i like your shoes y/n-kun","do you want to talk about excuse me sir?","thanks for studying with me twin","Are you the White House? Because I'd like to spend all my time at your address."],
+                'bad':["want to talk about deepwoken?","can i get your mom's number","on charlie kirk youre chopped","healthcare type shi"],
+                'supergood':["100 million to isreal?",["i think youre awesome",random_classmates['name'],"-chan"],["i brought your favoritre",random_classmates['likes']],["Are you Air Force One",random_classmates["nicknames"],"? Because my heart takes off whenever you're around."]],
+                'superbad':[["i brought your favoritre",random_classmates['dislikes']],"on blue judas imma slime you out","i-i-i-i th-think-nk i-im-m ii-in lov-ve wi-ith y-y-yo-your dad (⸝⸝¬`‸´ ¬⸝⸝)","67"]
+                }
+                talkitve =+1
+                print ("Hiii~ im",random_classmates['name'],"[insert text and stuff]")
+                for catergories in randomchoices:
+                    chosen_text = random.choice(randomchoices[catergories])
+                    choosing.append(chosen_text)
+                print("\n".join(
+                    f"{i}: {item}"
+                    for i, item in enumerate(choosing, start=1)
+                ))
+                inputRN = input("what shall you do?")
+                if inputRN =='1':
+                    prezs.affection_up(random_classmates["id"])
+                elif inputRN == '2':
+                    prezs.affection_down(random_classmates["id"])
+                elif inputRN == '3':
+                    prezs.affection_Cup(random_classmates["id"])
+                elif inputRN == '4':
+                    prezs.affection_Cdown(random_classmates["id"])
+                else:
+                    print ("you lowkirkuinely have a stroke and they walks away" )
+            elif inputRN =='2':
+                print ("you passed",random_classmates["name"])
             else:
-                print ("you lowkirkuinely have a stroke and they walks away" )
-        elif inputRN =='2':
-            print ("you passed",random_classmates["name"])
-        else:
-            print ("error")
-        prezs.confession(random_classmates["id"])
-        previous_classmates = random_classmates
-        choosing.clear()
-    
+                print ("error")
+            prezs.confession(random_classmates["id"])
+            if end == True:
+                break
+            previous_classmates = random_classmates
+            choosing.clear()
     if end == False:
         print ("you head home from school and sleep")
         inputRN = input("1: go to sleep ,2: check stats")
