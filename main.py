1852296723b5cb58c63efa9d3d65d3f17e60a68c
dan-te6741 <danieli96@nycstudents.net>
2026-06-05 10:16:21 -0400

    wgsdg

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import random
+lovers=[]
 inputRN = 0
 end = False
-class prezs:
+class prezs: #3
     def __init__(president,id,name,affection,personality,likes,dislikes,nickname):
         president.name = name
         president.id =id
@@ -10,9 +11,8 @@ class prezs:
         president.likes = likes
         president.dislikes = dislikes
         president.nickname = nickname
-    # def stats():
-    #     for Ps in Classmates:
-    #         print (Ps['name'])
+    def stats(president):
+        print("Name:", president.name,",Affection:", president.__affection,",Personality:", president.personality,",Likes:", president.likes,",Dislikes:", president.dislikes,",Nickname:", president.nickname)
     def affection_up(president):
         president.__affection +=10
         print (president.name,"'s affection for y/n is now",president.__affection,"<3" )
@@ -36,26 +36,37 @@ class prezs:
             elif inputRN == '2':
                 president.__affection -= 200
                 print("hell nah twin i see you as a brother")
-            else: 
+            else:
                 print ("you lowkey just die brochacho")
+
+
+
+
+
+
 
 
 previous_classmates = 0
 
-                
+
+               
 
 
-Trump = prezs(1,"Donald trump",-30,"tsundere",{"red caps", "tan spray","big arch"},{"learning"},"Trumpy-kun")
-Biden = prezs(2,"Joe biden",0 ,"forgetful",{"icecream", "his cildhood blanket 'blanky'"}, {"alarms"}, "sleepy senpai")
-Obama = prezs(3,"Barrack obama",20,"chill",{"beatboxxing","grillcheese"},{"talking loudly"}, "obama-san")
-Lincoln = prezs(4,"Abraham lincoln",10,"boxing tomboy but also theater kid",{"voting booths","musicals","dupliKate"},{"extortion","mustaches","omni-man"},"abby")
-Washington = prezs(5,"George washington",50,"wierd childhood friend",{"quarters","you <3","BL manga"},{"popcorn","the beatles"},"weirdo")
-Jfk = prezs(6,"John F Kennedy",-30,"popular kid",{"joyrides","talking"}, {"nerds"},"kenny-chan")
-Teddy = prezs(7,"Theodore Rossevelt",0,"baseball tomboy",{"baseball"},{"creepy crawlies","algebra"},"teddy bear")
+
+
+Trump = prezs(1,"Donald trump",-30,"tsundere","tan spray","learning","Trumpy-kun")
+Biden = prezs(2,"Joe biden",0 ,"forgetful","icecream", "alarms", "sleepy senpai")
+Obama = prezs(3,"Barrack obama",20,"chill","grillcheese","talking loudly", "obama-san")
+Lincoln = prezs(4,"Abraham lincoln",10,"boxing tomboy but also theater kid","musicals","omni-man","abby")
+Washington = prezs(5,"George washington",50,"wierd childhood friend","BL manga","the beatles","weirdo")
+Jfk = prezs(6,"John F Kennedy",-30,"popular kid","convertible","nerds","kenny-chan")
+Teddy = prezs(7,"Theodore Rossevelt",0,"baseball tomboy","triple T","creepy crawlies","teddy bear")
+
+
 
 
 Classmates = [
-{"id":Trump,"name":"Donald trump","personality":"tsundere","likes":"tan spray","dislikes":{"learning"},"nicknames":"Trumpy-kun"},
+{"id":Trump,"name":"Donald trump","personality":"tsundere","likes":"tan spray","dislikes":"learning","nicknames":"Trumpy-kun"},
 {"id":Biden,"name":"Joe biden","personality":"forgetful","likes":"icecream", "dislikes":"alarms", "nicknames":"sleepy senpai"},
 {"id":Obama,"name":"Barrack obama","personality":"chill","likes":"grillcheese","dislikes":"talking loudly", "nicknames":"obama-san"},
 {"id":Lincoln,"name":"Abraham lincoln","personality":"boxing tomboy but also theater kid","likes":"musicals","dislikes":"omni-man","nicknames":"abby"},
@@ -64,41 +75,63 @@ Classmates = [
 {"id":Teddy,"name":"Theodore Rossevelt","personality":"baseball tomboy","likes":"triple T","dislikes":"creepy crawlies","nicknames":"teddy bear"}
 ]
 random_classmates = random.choice(Classmates)
-randnmum = random.randint(1,3)
-randomchoices = {
-'good':["i like your shoes y/n-kun","do you want to talk about excuse me sir?","thanks for studying with me twin"],
-'bad':["want to talk about deepwoken?","can i get your mom's number","on charlie kirk youre chopped"],
-'supergood':["100 million to isreal?","i think youre awesome y/n-chan","i brought your favoritre",random_classmates['likes']],
-'superbad':["i brought your favoritre",random_classmates['dislikes'],"on blue judas imma slime you out","i-i-i-i th-think-nk i-im-m ii-in lov-ve wi-ith y-y-yo-your dad (⸝⸝¬`‸´ ¬⸝⸝)"]
-
-}
 
 
+choosing = []
+talkitve =0
+day_count = 0
 while not end:
-    print (randomchoices['good'])
-    random_classmates = random.choice(Classmates)
-    if random_classmates == previous_classmates:
-        while random_classmates == previous_classmates:
+    day_count +=1
+    print ("day",day_count)
+    if end == False:
+        for encounters in range(3):
             random_classmates = random.choice(Classmates)
-    print ("you encountered",random_classmates['name'], "in the hallway")
-    inputRN = input('1:hi or 2:ignore')
-    if inputRN =='1':
-        print ("Hiii~ im",random_classmates['name'],"[insert text and stuff]")
-        inputRN = input("1:*Malicously frame mog*,2: say 'my exp bar is low (๑/////๑')',3:*talk about deepwoken lore*,4: *give chocoloate*")
-        if inputRN =='1':
-            prezs.affection_down(random_classmates["id"])
-        elif inputRN == '2':
-            prezs.affection_up(random_classmates["id"])
-        elif inputRN == '3':
-            prezs.affection_Cdown(random_classmates["id"])
-        elif inputRN == '4':
-            prezs.affection_Cup(random_classmates["id"])
-        else:
-            print ("you lowkirkuinely have a stroke and they walks away" )
-    elif inputRN =='2':
-        print ("you passed",random_classmates["name"])
-    else:
-        print ("error")
-    prezs.confession(random_classmates["id"])
-    previous_classmates = random_classmates
-    # prezs.stats()
+            if random_classmates == previous_classmates:
+                while random_classmates == previous_classmates:
+                    random_classmates = random.choice(Classmates)
+            print ("you encountered",random_classmates['name'], "in the hallway")
+            inputRN = input('1:hi or 2:ignore')
+            if inputRN =='1':
+                randomchoices = {
+                'good':["i like your shoes y/n-kun","do you want to talk about excuse me sir?","thanks for studying with me twin","Are you the White House? Because I'd like to spend all my time at your address."],
+                'bad':["want to talk about deepwoken?","can i get your mom's number","on charlie kirk youre chopped","healthcare type shi"],
+                'supergood':["100 million to isreal?",["i think youre awesome",random_classmates['name'],"-chan"],["i brought your favoritre",random_classmates['likes']],["Are you Air Force One",random_classmates["nicknames"],"? Because my heart takes off whenever you're around."]],
+                'superbad':[["i brought your favoritre",random_classmates['dislikes']],"on blue judas imma slime you out","i-i-i-i th-think-nk i-im-m ii-in lov-ve wi-ith y-y-yo-your dad (⸝⸝¬`‸´ ¬⸝⸝)","67"]
+                }
+                talkitve =+1
+                print ("Hiii~ im",random_classmates['name'],"[insert text and stuff]")
+                for catergories in randomchoices:
+                    chosen_text = random.choice(randomchoices[catergories])
+                    choosing.append(chosen_text)
+                print("\n".join(
+                    f"{i}: {item}"
+                    for i, item in enumerate(choosing, start=1)
+                ))
+                inputRN = input("what shall you do?")
+                if inputRN =='1':
+                    prezs.affection_up(random_classmates["id"])
+                elif inputRN == '2':
+                    prezs.affection_down(random_classmates["id"])
+                elif inputRN == '3':
+                    prezs.affection_Cup(random_classmates["id"])
+                elif inputRN == '4':
+                    prezs.affection_Cdown(random_classmates["id"])
+                else:
+                    print ("you lowkirkuinely have a stroke and they walks away" )
+            elif inputRN =='2':
+                print ("you passed",random_classmates["name"])
+            else:
+                print ("error")
+            prezs.confession(random_classmates["id"])
+            if end == True:
+                break
+            previous_classmates = random_classmates
+            choosing.clear()
+    if end == False:
+        print ("you head home from school and sleep")
+        inputRN = input("1: go to sleep ,2: check stats")
+        if inputRN == "2":
+            for ppl in Classmates:
+                prezs.stats(ppl["id"])
+            print("youve talked",talkitve,"times in",day_count,"days")
+
